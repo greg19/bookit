7ce73cf6a2bc298594d8ccbeb4f144a1dc3dc81a
Grzegorz Kwacz <gkwacz@hotmail.com>
2023-01-14 17:29:16 +0100

    Example web interface

---
+++ reservations/src/main/java/com/bookit/reservations/AppController.java
@@ -0,0 +1,14 @@
+package com.bookit.offers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/")
+    public String test() {
+        return "test";
+    }
+
+}
